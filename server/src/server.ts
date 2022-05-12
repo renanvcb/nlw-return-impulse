5ed8a64a159e1bbe72a39dc1e61b1a1d8a91a563
Renan Borges <renanvcb@gmail.com>
2022-05-12 10:46:21 -0300

    server.ts being set

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,1 +1,12 @@
 import express from 'express';
+
+const port = 3333;
+const app = express();
+
+app.get('/users', (_req, res) => {
+  return res.send('Hello users!');
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
