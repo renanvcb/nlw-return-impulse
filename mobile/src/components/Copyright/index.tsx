cfb5b3f52ff03db34613ae5c49b199351b51db44
Renan Borges <renanvcb@gmail.com>
2022-05-17 16:59:28 -0300

    Copyright component created

---
+++ mobile/src/components/Copyright/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import {
+  Text,
+  View
+} from 'react-native';
+
+import { styles } from './styles';
+
+export function Copyright() {
+  return (
+    <View style={styles.container}>
+      <Text>
+
+      </Text>
+    </View>
+  );
+}
