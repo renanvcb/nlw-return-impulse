d803b8d56aa5fd9b177adabcc490b46714df0a23
Renan Borges <renanvcb@gmail.com>
2022-05-17 18:54:39 -0300

    take and remove screenshot functions added

--- mobile/src/components/Form/index.tsx
+++ mobile/src/components/Form/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   Image,
   Text,
@@ -7,6 +7,7 @@ import {
   View
 } from 'react-native';
 import { ArrowLeft } from 'phosphor-react-native';
+import { captureScreen } from 'react-native-view-shot';
 
 import { FeedbackType } from '../Widget';
 import { ScreenshotButton } from '../ScreenshotButton';
@@ -21,7 +22,22 @@ interface Props {
 }
 
 export function Form({ feedbackType }: Props) {
+  const [screenshot, setScreenshot] = useState<string | null>(null);
+
   const feedbackTypeInfo = feedbackTypes[feedbackType];
+
+  function handleTakeScreenshot() {
+    captureScreen({
+      format: 'jpg',
+      quality: 0.8,
+    })
+      .then(uri => setScreenshot(uri))
+      .catch(error => console.log(error));
+  }
+
+  function handleRemoveScreenshot() {
+    setScreenshot(null);
+  }
 
   return (
     <View style={styles.container}>
@@ -51,9 +67,9 @@ export function Form({ feedbackType }: Props) {
       />
       <View style={styles.footer}>
         <ScreenshotButton
-          onTakeShot={() => { /* implement */ }}
-          onRemoveShot={() => { /* implement */ }}
-          screenshot=''
+          onTakeShot={handleTakeScreenshot}
+          onRemoveShot={handleRemoveScreenshot}
+          screenshot={screenshot}
         />
         <SubmitButton isLoading={false} />
       </View>
