c9488eceb0a783d4e60ec1d4b44844ca029a3187
Renan Borges <renanvcb@gmail.com>
2022-05-17 19:07:03 -0300

    Selecting feedback type on Widget

--- mobile/src/components/Widget/index.tsx
+++ mobile/src/components/Widget/index.tsx
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import { TouchableOpacity } from 'react-native';
 import { ChatTeardropDots } from 'phosphor-react-native';
 import BottomSheet from '@gorhom/bottom-sheet';
@@ -15,6 +15,9 @@ import { feedbackTypes } from '../../utils/feedbackTypes';
 export type FeedbackType = keyof typeof feedbackTypes;
 
 function Widget() {
+  const [feedbackType, setFeedbackType] = useState<FeedbackType | null>(null);
+  const [isFeedbackSent, setIsFeedbackSent] = useState(false);
+
   const bottomSheetRef = useRef<BottomSheet>(null);
 
   function handleOpenBottomSheet() {
@@ -39,11 +42,20 @@ function Widget() {
         backgroundStyle={styles.modal}
         handleIndicatorStyle={styles.indicator}
       >
-        {/* <Options /> */}
-        {/* <Success /> */}
-        <Form
-          feedbackType='BUG'
-        />
+        {
+          isFeedbackSent ?
+            <Success />
+            :
+            <>
+              {
+                feedbackType ?
+                  <Form
+                    feedbackType={feedbackType}
+                  /> :
+                  <Options onFeedbackTypeChanged={setFeedbackType} />
+              }
+            </>
+        }
       </BottomSheet>
     </>
   );
